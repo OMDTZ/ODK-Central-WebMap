7015e83929e60a1df3547f73ee78c4412f5c8228
OMDTZ <omdtanzania@gmail.com>
2022-05-12 10:02:03 +0300

    Move webmap files in to nginx folder

--- script/setup.sh
+++ script/setup.sh
@@ -3,4 +3,14 @@
 # Set up a server
 
   sudo apt install nginx -y
+
+
+  # Move webmap files in nginx folder
+  # This is temporary
+
+  sudo cp app/index.html /var/www/html/
+  sudo cp app/index.js /var/www/html/
+  sudo cp app/static/styles.css /var/www/html/
+  sudo cp -r app/data /var/www/html/
+  sudo cp -r app/Leaflet.markercluster-1.4.1 /var/www/html/
   
