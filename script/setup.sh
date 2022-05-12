10b9b28d018f430cff71a41f4cdaf499eba6b039
timlamec <55591841+timlamec@users.noreply.github.com>
2022-05-12 10:36:48 +0300

    added css folder

--- script/setup.sh
+++ script/setup.sh
@@ -13,4 +13,6 @@
   sudo cp app/static/styles.css /var/www/html/
   sudo cp -r app/data /var/www/html/
   sudo cp -r app/Leaflet.markercluster-1.4.1 /var/www/html/
+  sudo cp -r app/css /var/www/html/
+  
   
