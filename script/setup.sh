0db3812491ba66100776f65039efa915e6e708af
timlamec <timlamec@gmail.com>
2022-05-19 09:53:52 +0300

    added encryption to setup script

--- script/setup.sh
+++ script/setup.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-# Set up a server
+# Sets up an ODK webmap server.
 
-  sudo apt install nginx -y
+# Assumes a non-root sudo user called webmap.
+
+echo please enter the domain name of your server
+read domain_name
+echo
+echo Please enter an email address for certificate renewal information
+read email
+echo
+echo Updating and upgrading the OS
+sudo apt -y update
+sudo apt -y upgrade
+
+
+
+  echo installing nginx
+if ! type "nginx"; then
+    sudo apt install -y nginx
+else echo Nginx seems to be already installed
+fi
+
+echo installing Certbot
+if ! type "certbot"; then
+    sudo apt install -y certbot python3-certbot-nginx
+else echo Certbot seems to be already installed
+fi
+
+echo Procuring a certificate for the site from LetsEncrypt using Certbot
+sudo certbot --nginx -n --agree-tos --redirect -m $email -d $domain_name -d www.$domain_name
 
 
   # Move webmap files in nginx folder
